d8a3728a13beda3bdd8815f6f8904458326b4b17
BaldurThor <baldurthor96@gmail.com>
2025-01-07 21:22:02 +0000

    small change to print surface

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1724,12 +1724,12 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 190
+#define X_BED_SIZE 232
+#define Y_BED_SIZE 193
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -40
+#define Y_MIN_POS -22
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
